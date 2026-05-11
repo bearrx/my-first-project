027e18af148247cb53dd77a53a54f8cc98f58944
Cassey Tumwi <kctumwi@gmail.com>
2026-05-11 21:25:27 +0300

    Add localStorage to persist data

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const button = document.getElementById("btn");
 const text = document.getElementById("text");
 
+// Load saved value when page opens
+const savedText = localStorage.getItem("message");
+
+if (savedText) {
+  text.textContent = savedText;
+}
+
+// When button is clicked
 button.addEventListener("click", function () {
-  text.textContent = "You clicked the button 🎉";
+  const newMessage = "You clicked the button 🎉";
+
+  text.textContent = newMessage;
+
+  // Save to localStorage
+  localStorage.setItem("message", newMessage);
 });
